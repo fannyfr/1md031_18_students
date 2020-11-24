314677eda9357ab1c24df0b8c1f892e82e908975
fannyfr <fanny_fristedt@hotmail.se>
2020-11-24 13:41:00 +0100

    blä

---
+++ public/js/menu.js
@@ -0,0 +1,26 @@
+var food = [
+  {
+  id:1,
+  name: "God Hamburgare",
+  kCal: 50,
+  lactose: true,
+  gluten: true,
+  bild: "https://www.max.se/contentassets/bd97342be78047eb8f8026452d545538/product_halloumiburgare.png?width=1024&sharpen=5&sigma=1,4&threshold=0"
+},
+{
+  id:2,
+  name: "Glutenfri burgare",
+  kCal : 0,
+  lactose: false,
+  gluten: false,
+  bild:"https://pagen.se/globalassets/kampanjer/garden-party-2018/burgare-med-vitloks--och-chilimajonas.png.png?w=734&h=462&mode=crop&resized=true"
+},
+{
+  id:3,
+  name:"Firma Burgare",
+  kCal: 1000000,
+  lactose: true,
+  gluten: true,
+  bild: "https://pagen.se/globalassets/kampanjer/garden-party-2018/burgare-med-vitloks--och-chilimajonas.png.png?w=734&h=462&mode=crop&resized=true"
+}
+]
---
+++ public/js/vue_script.js
@@ -0,0 +1,13 @@
+
+var mh = new Vue({
+  el: '#mainID',
+  data: {
+    theburgers: food,
+  },
+  methods:{
+  sendOrder: function()
+  {
+    consol.log("smask vad gott")
+  }
+  }
+});
